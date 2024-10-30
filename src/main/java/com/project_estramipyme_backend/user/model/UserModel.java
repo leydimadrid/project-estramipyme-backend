2156af6c9ff5c7b11cf21baa11ae640ff17867a4
DiegoHernandez <diego0930@gmail.com>
2024-10-30 13:53:03 -0500

    Modificar la clase Person para implementar UserDetails

--- src/main/java/com/project_estramipyme_backend/user/model/UserModel.java
+++ src/main/java/com/project_estramipyme_backend/user/model/UserModel.java
@@ -1,11 +1,16 @@
 package com.project_estramipyme_backend.user.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,7 +18,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class UserModel {
+public class UserModel implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,7 +30,45 @@ public class UserModel {
     private String businessName;
     private String sector;
     private String otherSector;
+    @Column(unique = true)  // Asegura que el email sea único
     private String email;
-    private String password;
+    @JsonIgnore
+    private String password;  // Asegura que la contraseña esté oculta al serializar la entidad
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Devuelve la lista de roles o permisos del usuario. Puedes retornar una lista vacía si no estás usando roles.
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;  // Usa el correo electrónico como nombre de usuario
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;  // Puedes agregar lógica para determinar si la cuenta está expirada
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;  // Puedes agregar lógica para determinar si la cuenta está bloqueada
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;  // Puedes agregar lógica para verificar si las credenciales han expirado
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;  // Puedes agregar lógica para determinar si el usuario está habilitado
+    }
 }
