0a02a17c5bdd3869ca939609711503dcb56e0a14
leydimadrid <leydi.madrid6@gmail.com>
2024-11-08 18:08:43 -0500

    Integración rama Liz

--- src/main/java/com/project_estramipyme_backend/Security/controllers/AuthController.java
+++ src/main/java/com/project_estramipyme_backend/Security/controllers/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//Swagger
 @Tag(name = "Autenticación", description = "API de autenticación de usuarios")
 @RestController
 @RequestMapping("api/auth")
