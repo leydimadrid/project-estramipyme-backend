8986cc9d73d125320b3a0b0616a8c272feb21ed3
DiegoHernandez <diego0930@gmail.com>
2024-11-08 09:29:33 -0500

    JwtUtilRec util para la creacion de tokens para la recuperacion de contraseña

---
+++ src/main/java/com/project_estramipyme_backend/Security/security/JwtUtilRec.java
@@ -0,0 +1,34 @@
+package com.project_estramipyme_backend.Security.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtUtilRec {
+    private String SECRET_KEY = "mysecretkey";
+
+    // Método para generar token JWT para recuperación de contraseña
+    public String generateTokenForPasswordReset(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))  // Expira en 30 minutos
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
