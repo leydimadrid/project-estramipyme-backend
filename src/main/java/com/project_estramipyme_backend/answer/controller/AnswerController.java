073e7b10f1e2502d7cdb9db1d589f4e13cc63258
mcamilabotero3 <mcboterop@eafit.edu.co>
2024-11-11 20:16:00 -0500

    Update AnswerController.java

--- src/main/java/com/project_estramipyme_backend/answer/controller/AnswerController.java
+++ src/main/java/com/project_estramipyme_backend/answer/controller/AnswerController.java
@@ -2,13 +2,15 @@ package com.project_estramipyme_backend.answer.controller;
 
 import com.project_estramipyme_backend.answer.model.AnswerModel;
 import com.project_estramipyme_backend.answer.service.AnswerService;
-import com.project_estramipyme_backend.form.model.FormModel;
-import com.project_estramipyme_backend.user.model.UserModel;
+import com.project_estramipyme_backend.answer.service.PDFService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -17,11 +19,15 @@ import java.util.Optional;
 @RequestMapping("/api/answers")
 public class AnswerController {
     private final AnswerService answerService;
+    private final PDFService pdfService;
 
     @Autowired
-    public AnswerController(AnswerService answerService) {
+    public AnswerController(AnswerService answerService, PDFService pdfService) {
         this.answerService = answerService;
+        this.pdfService = pdfService;
     }
+
+
 
 
     @GetMapping(path = "/getAnswers")
@@ -39,5 +45,26 @@ public class AnswerController {
         return this.answerService.getAnswerById(id);
     }
 
-}
+    @GetMapping(path = "/generatePdf")
+    public ResponseEntity<byte[]> generatePdf() {
+        List<AnswerModel> answers = this.answerService.getAllAnswers();
 
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            pdfService.generatePdf(answers, baos);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "attachment; filename=answers.pdf");
+
+            return ResponseEntity
+                    .ok()
+                    .headers(headers)
+                    .contentType(org.springframework.http.MediaType.APPLICATION_PDF)
+                    .body(baos.toByteArray());
+
+        } catch (FileNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
