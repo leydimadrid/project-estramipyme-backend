cfa5ad7ac699ff8e89543930765c0b495cdfca8b
Cristian-Zuluaga <cdzg8249@gmail.com>
2024-11-19 20:43:11 -0500

    Logica graficos api

--- src/main/java/com/project_estramipyme_backend/test/repository/ITestRepository.java
+++ src/main/java/com/project_estramipyme_backend/test/repository/ITestRepository.java
@@ -1,6 +1,7 @@
 package com.project_estramipyme_backend.test.repository;
 
 import com.project_estramipyme_backend.test.dto.InfoEsquemaReoDTO;
+import com.project_estramipyme_backend.test.dto.InfoResultadoCirculoDoradoDTO;
 import com.project_estramipyme_backend.test.model.TestModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,4 +23,37 @@ public interface ITestRepository extends JpaRepository<TestModel, Long> {
           GROUP BY f.id, f.name
           """)
     List<InfoEsquemaReoDTO> getReportEsquemaReo(@Param("testId") Long testId);
+
+    @Query("""
+            SELECT new com.project_estramipyme_backend.test.dto.InfoResultadoCirculoDoradoDTO('Por Que', (SUM(qo.score) * 100 / 16))
+            FROM TestModel t
+            JOIN AnswerModel ao ON ao.test = t
+            JOIN Question_Option qo ON qo = ao.question_option
+            JOIN Question q ON q = qo.question
+            JOIN FormModel f ON f = q.form
+            WHERE t.id = :testId AND f.id = 6 AND q.id IN(50,51,52,53)
+        """)
+    InfoResultadoCirculoDoradoDTO getPorqueInfoCirculo(@Param("testId") Long testId);
+
+    @Query("""
+            SELECT new com.project_estramipyme_backend.test.dto.InfoResultadoCirculoDoradoDTO('Como', (SUM(qo.score) * 100 / 16))
+            FROM TestModel t
+            JOIN AnswerModel ao ON ao.test = t
+            JOIN Question_Option qo ON qo = ao.question_option
+            JOIN Question q ON q = qo.question
+            JOIN FormModel f ON f = q.form
+            WHERE t.id = :testId AND f.id = 6 AND q.id IN(54,55,56,57)
+        """)
+    InfoResultadoCirculoDoradoDTO getComoInfoCirculo(@Param("testId") Long testId);
+
+    @Query("""
+            SELECT new com.project_estramipyme_backend.test.dto.InfoResultadoCirculoDoradoDTO('Que', (SUM(qo.score) * 100 / 16))
+            FROM TestModel t
+            JOIN AnswerModel ao ON ao.test = t
+            JOIN Question_Option qo ON qo = ao.question_option
+            JOIN Question q ON q = qo.question
+            JOIN FormModel f ON f = q.form
+            WHERE t.id = :testId AND f.id = 6 AND q.id IN(58,59,60,61)
+        """)
+    InfoResultadoCirculoDoradoDTO getQueInfoCirculo(@Param("testId") Long testId);
 }
--- src/main/java/com/project_estramipyme_backend/test/service/TestService.java
+++ src/main/java/com/project_estramipyme_backend/test/service/TestService.java
@@ -1,6 +1,7 @@
 package com.project_estramipyme_backend.test.service;
 
 import com.project_estramipyme_backend.test.dto.InfoEsquemaReoDTO;
+import com.project_estramipyme_backend.test.dto.InfoResultadoCirculoDoradoDTO;
 import com.project_estramipyme_backend.test.model.TestModel;
 import com.project_estramipyme_backend.test.repository.ITestRepository;
 import com.project_estramipyme_backend.user.model.UserModel;
@@ -34,4 +35,15 @@ public class TestService {
         return testRepository.getReportEsquemaReo(formId);
     }
 
+    public InfoResultadoCirculoDoradoDTO getPorqueInfoCirculo(Long formId){
+        return testRepository.getPorqueInfoCirculo(formId);
+    }
+
+    public InfoResultadoCirculoDoradoDTO getComoInfoCirculo(Long formId){
+        return testRepository.getComoInfoCirculo(formId);
+    }
+
+    public InfoResultadoCirculoDoradoDTO getQueInfoCirculo(Long formId){
+        return testRepository.getQueInfoCirculo(formId);
+    }
 }
