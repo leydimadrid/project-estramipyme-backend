a16d5d5a77da739f02ef19732decf35ec5b14673
leydimadrid <leydi.madrid6@gmail.com>
2024-11-24 18:38:12 -0500

    Ajuste cors

--- src/main/java/com/project_estramipyme_backend/config/WebConfig.java
+++ src/main/java/com/project_estramipyme_backend/config/WebConfig.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://estramipyme-web.netlify.app", "http://localhost:4200", "https://project-estramipyme-backend-production.up.railway.app");
+        registry.addMapping("/**")
+                .allowedOrigins("https://estramipyme-web.netlify.app", "http://localhost:4200", "https://project-estramipyme-backend-production.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
