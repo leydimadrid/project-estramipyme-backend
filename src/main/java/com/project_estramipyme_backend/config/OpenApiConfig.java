cecef27db78c483a9dd031fe78bcfcbe9b4e9604
leydimadrid <leydi.madrid6@gmail.com>
2024-11-25 12:27:06 -0500

    Traducciòn algunos textos swagger

--- src/main/java/com/project_estramipyme_backend/config/OpenApiConfig.java
+++ src/main/java/com/project_estramipyme_backend/config/OpenApiConfig.java
@@ -5,10 +5,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 import java.util.Arrays;
+
 
 @Configuration
 public class OpenApiConfig {
@@ -18,12 +21,12 @@ public class OpenApiConfig {
 
         Server productionServer = new Server()
                 .url("https://project-estramipyme-backend-production.up.railway.app")
-                .description("Servidor de Producción - Railway");
+                .description("Production Server - Railway");
 
 
         Server localServer = new Server()
                 .url("http://localhost:8081")
-                .description("Servidor Local - Desarrollo");
+                .description("Local Server - Development");
 
 
         return new OpenAPI()
@@ -31,12 +34,14 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Estramipyme API")
                         .version("1.0")
-                        .description("API de Evaluación Empresarial"))
+                        .description("Enterprise Assessment API"))
                 .components(new Components()
                         .addSecuritySchemes("bearer-jwt", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")))
-         .servers(Arrays.asList(productionServer, localServer)); // Agregar los servidores
+                .servers(Arrays.asList(productionServer, localServer)); // Agregar los servidores
     }
+
+
 }
